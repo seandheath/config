a259224f2c8af11a5bec251de2abdfbaf4e4e610
Sean Heath <se@nheath.com>
2020-05-12 20:53:45 -0400

    post install on laptop

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eo pipefail
+
 WD=$(pwd)
 
 if [ $(hostname) = "localhost" ]; then
@@ -17,8 +19,14 @@ say(){
 
 if [ ! -f /etc/yum.repos.d/rpmfusion-free.repo ]; then
 	say "Installing RPM Fusion"
-	sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+	sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 fi
+
+say "Installing profile-sync-daemon"
+if [ ! -f /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:szasza:Profile-sync-daemon.repo ]; then
+    sudo dnf copr enable -y szasza/Profile-sync-daemon
+fi
+
 
 if [ ! -f /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:pschyska:alacritty.repo ]; then
 	say "Adding COPR repository for Alacritty"
@@ -32,7 +40,7 @@ fi
 
 if [ ! -f /etc/yum.repos.d/vscodium.repo ]; then
 	say "Adding VSCodium Repository"
-	sudo rpm --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
+	sudo rpm --import -y https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
 	printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=gitlab.com_paulcarroty_vscodium_repo\nbaseurl=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/repos/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg" |sudo tee -a /etc/yum.repos.d/vscodium.repo
 fi
 
@@ -60,7 +68,9 @@ cp files/alacritty.yml ~/.config/alacritty/
 say "Updating i3"
 mkdir -p ~/.config/i3
 cp files/i3config ~/.config/i3/config
-cat files/i3config-$(hostname) >> ~/.config/i3/config
+if [ -f files/i3-config$(hostname) ]; then
+	cat files/i3config-$(hostname) >> ~/.config/i3/config
+fi
 
 say "Updating i3status"
 mkdir -p ~/.config/i3status
@@ -78,7 +88,10 @@ sudo cp files/99-powertargets.rules /etc/udev/rules.d/
 sudo systemctl enable --now governor.service
 
 say "Updating flatpaks"
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+say "Enabling psd"
+systemctl --user enable --now psd
 
 say "Installing pdfannots"
 sudo cp files/pdfannots /usr/bin/pdfannots
@@ -89,14 +102,6 @@ sudo systemctl disable --now nordvpnd
 
 say "Disabling libvirtd"
 sudo systemctl disable --now libvirtd
-
-say "Installing profile-sync-daemon"
-if [ ! -f /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:szasza:Profile-sync-daemon.repo ]; then
-    sudo dnf copr enable szasza/Profile-sync-daemon
-    sudo dnf install profile-sync-daemon
-    systemctl --user enable --now psd
-fi
-
 
 if [ ! -f /usr/bin/vim ]; then
     say "Linking nvim to vim"
@@ -123,12 +128,13 @@ else
 	say "Rust already installed"
 fi
 
-if [ ! -d ~/.config/nvm ]; then
+if [ ! -d ~/.nvm ]; then
 	say "Installing NVM"
 	curl -sSL -o /tmp/nvm.sh https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh
 	chmod +x /tmp/nvm.sh
 	/tmp/nvm.sh
 	source ~/.bashrc
+	say "Installing node"
 	nvm install node
 else
 	say "NVM already installed"
@@ -136,6 +142,8 @@ fi
 
 if [ ! -d ~/.rvm ]; then
 	say "Installing RVM"
+	curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
+	curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
 	curl -sSL -o /tmp/rvm.sh https://get.rvm.io
 	chmod +x /tmp/rvm.sh
 	/tmp/rvm.sh stable --ruby
